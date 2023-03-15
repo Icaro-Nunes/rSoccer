0238d45d761ce7335eb1ef82549a20c9f9526db3
Icaro-Nunes <icarolimanunes@gmail.com>
2023-03-15 13:55:01 -0300

    Made y_dist_penalty smaller

--- rsoccer_gym/vss/env_gk/vss_gk.py
+++ rsoccer_gym/vss/env_gk/vss_gk.py
@@ -458,7 +458,7 @@ class rSimVSSGK(VSSBaseEnv):
         w_move = 0.2
         w_ball_pot = 0.1
         w_move_y = 0.0
-        w_y_dist_penalty = 0.6
+        w_y_dist_penalty = 0.3
         w_distance = 0.1
         w_blva = 2.0
         w_angle = 0.1
