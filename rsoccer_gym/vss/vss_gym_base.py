80b0e97445c08a6f19c11c2c1f9ab9a7e4cf8ef2
Icaro-Nunes <icarolimanunes@gmail.com>
2023-06-01 20:59:30 -0300

    Fix denorm - the denorm factors on the environment were not being updated on reset

--- rsoccer_gym/vss/vss_gym_base.py
+++ rsoccer_gym/vss/vss_gym_base.py
@@ -81,6 +81,15 @@ class VSSBaseEnv(gym.Env):
         initial_pos_frame: Frame = self._get_initial_positions_frame()
         self.rsim.reset(initial_pos_frame, params)
 
+        # Get field dimensions
+        self.field = self.rsim.get_field_params()
+        self.max_pos = max(self.field.width / 2, (self.field.length / 2) 
+                                + self.field.penalty_length)
+        max_wheel_rad_s = (self.field.rbt_motor_max_rpm / 60) * 2 * np.pi
+        self.max_v = max_wheel_rad_s * self.field.rbt_wheel_radius
+        # 0.04 = robot radius (0.0375) + wheel thicknees (0.0025)
+        self.max_w = np.rad2deg(self.max_v / 0.04)
+
         # Get frame from simulator
         self.frame = self.rsim.get_frame()
 
