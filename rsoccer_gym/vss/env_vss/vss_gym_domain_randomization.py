a1c740d4bf0210bec1704d088cf2aabfa35b9c77
Icaro-Nunes <icarolimanunes@gmail.com>
2023-06-01 19:41:48 -0300

    Implemented uniform randomization on dynamics parameters, using wider ranges than before

--- rsoccer_gym/vss/env_vss/vss_gym_domain_randomization.py
+++ rsoccer_gym/vss/env_vss/vss_gym_domain_randomization.py
@@ -10,16 +10,21 @@ class VSSDomainRandomizationEnv(VSSEnv):
             "robot_wheel_tangent_friction": 0.8
         }
 
-        self.params = self.default_params.copy()
+        self.params_intervals = {
+            "robot_body_mass": (0.100, 0.300),
+            "robot_wheel_motor_max_rpm": (390.0, 520.0),
+            "robot_wheel_perpendicular_friction": (0.7, 1.0),
+            "robot_wheel_tangent_friction": (0.6, 0.95)
+        }
 
         super().__init__(self.generate_params())
 
     def generate_params(self):
-        for key in self.params:
-            val = self.default_params[key]
-            scale = val/10
-            self.params[key] = np.random.normal(val, scale)
-        return self.params
+        params = self.default_params.copy()
+        for key in params:
+            low, high = self.params_intervals[key]
+            params[key] = np.random.uniform(low, high)
+        return params
 
     def reset(self):
         return super().reset(self.generate_params())
